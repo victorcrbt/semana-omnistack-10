cfaccdf23ca3166cbb8bbec4fbf84098ed1eed11
Victor Batalha <madivcb@gmail.com>
2020-01-14 20:45:04 -0300

    created route to list all devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,6 @@ import DevController from './app/controllers/DevController';
 const routes = Router();
 
 routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
 
 export default routes;

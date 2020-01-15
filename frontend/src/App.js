acfd7549682a79001741b99bad5163b5d9c533fd
Victor Batalha <madivcb@gmail.com>
2020-01-15 14:57:14 -0300

    defined global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Main from './pages/Main';
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Main />
+
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;

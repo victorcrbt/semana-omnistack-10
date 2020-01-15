2fd806fce1fa1bce516c5345b035dabec2dedba1
Victor Batalha <madivcb@gmail.com>
2020-01-14 21:02:28 -0300

    create route to search devs for location and techs

---
+++ backend/src/app/controllers/SearchController.js
@@ -0,0 +1,29 @@
+import parseStringAsArray from '../../utils/parseStringAsArray';
+import Dev from '../models/Dev';
+
+class SearchController {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+
+    const techsArray = parseStringAsArray(techs, ',');
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    return res.json(devs);
+  }
+}
+
+export default new SearchController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import DevController from './app/controllers/DevController';
+import SearchController from './app/controllers/SearchController';
 
 const routes = Router();
 
 routes.post('/devs', DevController.store);
 routes.get('/devs', DevController.index);
+
+routes.get('/search', SearchController.index);
 
 export default routes;

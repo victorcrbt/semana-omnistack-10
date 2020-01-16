1c11b6e471e40375ed99008282cbb75b00c64eea
Victor Batalha <madivcb@gmail.com>
2020-01-16 20:23:50 -0300

    auth middleware setup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,14 +3,16 @@ import { Router } from 'express';
 import DevController from './app/controllers/DevController';
 import SearchController from './app/controllers/SearchController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/sessions', SessionController.store);
-
 routes.post('/devs', DevController.store);
-routes.get('/devs', DevController.index);
 
 routes.get('/search', SearchController.index);
+
+routes.use(authMiddleware);
+routes.get('/devs', DevController.index);
 
 export default routes;

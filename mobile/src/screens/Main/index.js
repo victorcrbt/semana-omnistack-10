5fb819c36c7da7316f6acfb3053c0201f805c1e2
Victor Batalha <madivcb@gmail.com>
2020-01-16 14:43:33 -0300

    map and marker stylization

--- mobile/src/screens/Main/index.js
+++ mobile/src/screens/Main/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Marker, Callout } from 'react-native-maps';
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
-// import { Container } from './styles';
+import { Map, Avatar, DevInfo, DevName, DevBio, DevTechs } from './styles';
 
 export default function Main() {
-  return <View />;
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (!granted) return;
+
+      const { coords } = await getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+
+      const { latitude, longitude } = coords;
+
+      setLocation({
+        latitude: -29.9001409,
+        longitude: -50.262451,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.04,
+      });
+    })();
+  }, []);
+
+  if (!location) {
+    return null;
+  }
+
+  return (
+    <Map initialRegion={location}>
+      <Marker coordinate={{ latitude: -29.9001409, longitude: -50.262451 }}>
+        <Avatar
+          source={{
+            uri: 'https://avatars1.githubusercontent.com/u/26396515?s=460&v=4',
+          }}
+        />
+
+        <Callout>
+          <DevInfo>
+            <DevName>Victor Batalha</DevName>
+            <DevBio>
+              Desenvolvedor Fullstack Javascript. NodeJS, ReactJS e React
+              Native. Sempre buscando aprimorar os conhecimentos!
+            </DevBio>
+            <DevTechs>Node.js, ReactJS, React Native</DevTechs>
+          </DevInfo>
+        </Callout>
+      </Marker>
+    </Map>
+  );
 }

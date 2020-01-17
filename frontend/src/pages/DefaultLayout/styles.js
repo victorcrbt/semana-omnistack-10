7286c394965c3ecfd197ad31d69cbe32e1d1630c
Victor Batalha <madivcb@gmail.com>
2020-01-16 21:14:32 -0300

    defined default layout

---
+++ frontend/src/pages/DefaultLayout/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 60px 30px;
+
+  display: flex;
+  align-items: flex-start;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`;

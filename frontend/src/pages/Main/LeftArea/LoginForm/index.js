a2331f2837ab06214df37261b81f0224f7795f0e
Victor Batalha <madivcb@gmail.com>
2020-01-16 22:28:25 -0300

    user authentication

---
+++ frontend/src/pages/Main/LeftArea/LoginForm/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext } from 'react';
+
+import AuthContext from '~/AuthContext';
+import api from '~/services/api';
+
+import { Container } from './styles';
+
+export default function LoginForm() {
+  const context = useContext(AuthContext);
+
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState({
+    status: false,
+    message: '',
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setError({
+      status: false,
+      message: '',
+    });
+
+    try {
+      const response = await api.post('/sessions', { username, password });
+
+      const { username: github_username, token } = response.data;
+
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+      context.setAuth({
+        ...context,
+        github_username,
+        signed: true,
+      });
+
+      setUserName('');
+      setPassword('');
+    } catch (err) {
+      if (err.response.status === 401) {
+        setError({
+          status: true,
+          message: 'Usuário ou senha inválidos.',
+        });
+      } else {
+        setError({
+          status: true,
+          message: 'Algo de errado aconteceu :/',
+        });
+      }
+    }
+  }
+
+  return (
+    <Container onSubmit={handleSubmit} error={error.status}>
+      <div className="error">{error.status && error.message}</div>
+
+      <div className="input-block">
+        <label htmlFor="username">Usuário</label>
+        <input
+          value={username}
+          onChange={e => setUserName(e.target.value)}
+          name="username"
+          id="username"
+          required
+        />
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          name="password"
+          id="password"
+          required
+        />
+      </div>
+      <button type="submit">Salvar</button>
+    </Container>
+  );
+}
